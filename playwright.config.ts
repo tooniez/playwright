2abb4022dc6a0c790bd8ccb5a93430957bdb03fe
toon <tluu08@gmail.com>
2022-06-25 14:07:32 +0930

    set output path

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ import { devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
+  outputDir: 'reports/',
   testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
@@ -97,8 +98,7 @@ const config: PlaywrightTestConfig = {
     // },
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+
 
   /* Run your local dev server before starting the tests */
   // webServer: {
